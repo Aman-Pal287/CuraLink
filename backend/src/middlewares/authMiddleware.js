7036018d36d844f29b89861daebc8c2ca8dd459f
Aman-Pal287 <palaman9786@gmail.com>
2025-10-31 22:10:06 +0530

    auth middle ware

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
+
+exports.protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ msg: "No token" });
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; // { id, role, iat, exp }
+    next();
+  } catch (err) {
+    return res.status(401).json({ msg: "Token invalid" });
+  }
+};
